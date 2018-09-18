ed6fef0a597bd81a2534af571fb1198fc3fa4659
ashiqsulaiman93 <ashiq@nestaway.com>
2018-09-18 18:52:55 +0530

    created header and image card

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+
+
+const Button = () => {
+    return (
+        <Text>Click me!</Text>
+    );
+};
+
+
+
+
+export default Button;
---
+++ src/components/CardSection.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View} from 'react-native';
+import Card from './Card';
+
+const CardSection = (props) => {
+    return (
+        <View style= {styles.containerStyle}>
+            {props.children}
+        </View>
+    );
+};
+
+
+const styles =  {
+    containerStyle: {
+        borderBottomWidth : 1,
+        padding: 5,
+        backgroudColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderColor: '#ddd',
+        position: 'relative',
+    }
+};
+
+
+export default CardSection;
